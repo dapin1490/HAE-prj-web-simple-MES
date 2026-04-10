d943dad8ff02717b73c9de02faf6c2e4830b93c7
junson <leej04652@gmail.com>
2026-04-10 16:01:46 +0900

    fix: H2 database Driver 오류해결

--- be-mes-project/src/main/java/group1/be_mes_project/BeMesProjectApplication.java
+++ be-mes-project/src/main/java/group1/be_mes_project/BeMesProjectApplication.java
@@ -2,8 +2,10 @@ package group1.be_mes_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class BeMesProjectApplication {
 
 	public static void main(String[] args) {
